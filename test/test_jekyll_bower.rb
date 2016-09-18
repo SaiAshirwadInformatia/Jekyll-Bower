969723bf0353e6bb927936716bea6063e47f8f60
Rohan Sakhale <rs@saiashirwad.com>
2016-09-18 15:54:00 +0530

    Updated test

--- test/test_jekyll_bower.rb
+++ test/test_jekyll_bower.rb
@@ -1,9 +1,10 @@
 # Test Google Analytics Tag
 require 'minitest/autorun'
-require 'jekyll_bower'
+require 'jekyll-bower'
 
 class JekyllBowerTest <  Minitest::Unit::TestCase
 	def test_jekyll_bower
+		Jekyll::Bower.new()
 		print system('which bower')
 	end
 end
